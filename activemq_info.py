dd2b8bbd6d9b7298f8e30a04b9cbb6ebf5bf08a0
Denis Zhdanov <dzhdanov@ebay.com>
2013-03-11 12:48:59 +0100

    migrated to OO style, bugfixes

--- activemq_info.py
+++ activemq_info.py
@@ -11,81 +11,79 @@ from xml.dom import minidom
 import urllib
 
 
-# Host to connect to. Override in config by specifying 'Host'.
-AMQ_ADMIN_HOST = 'localhost'
+class AMQMonitor(object):
 
-# Port to connect on. Override in config by specifying 'Port'.
-AMQ_ADMIN_PORT = 8161
+    def __init__(self):
+        self.plugin_name = "activemq_info"
+        self.amq_admin_host = 'localhost'
+        self.amq_admin_port = 8161
+        self.verbose_logging = False
 
-# Verbose logging on/off. Override in config by specifying 'Verbose'.
-VERBOSE_LOGGING = False
+    def log_verbose(self, msg):
+        if not self.verbose_logging:
+            return
+        collectd.info('activemq_info plugin [verbose]: %s' % msg)
 
-
-def log_verbose(msg):
-    if not VERBOSE_LOGGING:
-        return
-    collectd.info('activemq_info plugin [verbose]: %s' % msg)
-
-
-def configure_callback(conf):
-    """Receive configuration block"""
-    global AMQ_ADMIN_HOST, AMQ_ADMIN_PORT, VERBOSE_LOGGING
-    for node in conf.children:
-        if node.key == 'Host':
-            AMQ_ADMIN_HOST = node.values[0]
-        elif node.key == 'Port':
-            AMQ_ADMIN_PORT = int(node.values[0])
-        elif node.key == 'Verbose':
-            VERBOSE_LOGGING = bool(node.values[0])
-        else:
-            collectd.warning('activemq_info plugin: Unknown config key: %s.'
-                             % node.key)
-    log_verbose('Configured with host=%s, port=%s' % (AMQ_ADMIN_HOST, AMQ_ADMIN_PORT))
+    def configure_callback(self, conf):
+        """Receive configuration block"""
+        for node in conf.children:
+            if node.key == 'Host':
+                self.amq_admin_host = node.values[0]
+            elif node.key == 'Port':
+                self.amq_admin_port = int(node.values[0])
+            elif node.key == 'Verbose':
+                self.verbose_logging  = bool(node.values[0])
+            else:
+                collectd.warning('activemq_info plugin: Unknown config key: %s.' % node.key)
+        self.log_verbose('Configured with host=%s, port=%s' % (self.amq_admin_host, self.amq_admin_port))
 
 
-def dispatch_value(instance, key, value, value_type):
-    """Dispatch a value to collectd"""
-    log_verbose('Sending value: %s.%s=%s' % (instance, key, value))
-    val = collectd.Values(plugin='activemq_info')
-    val.plugin_instance = instance
-    val.type = value_type
-    val.values = [value]
-    val.dispatch()
+    def dispatch_value(self, plugin_instance, value_type, instance, value):
+        """Dispatch a value to collectd"""
+        self.log_verbose('Sending value: %s.%s.%s=%s' % (self.plugin_name, plugin_instance, instance, value))
+        val = collectd.Values()
+        val.plugin = self.plugin_name
+        val.plugin_instance = plugin_instance
+        val.type = value_type
+        val.type_instance = instance
+        val.values = [value, ]
+        val.dispatch()
 
 
-def fetch_info():
-    """Connect to ActiveMQ admin webpage and return DOM object"""
-    url = 'http://%s:%s/admin/xml/queues.jsp' % (AMQ_ADMIN_HOST, AMQ_ADMIN_PORT)
-    dom = None
-    try:
-        dom = minidom.parse(urllib.urlopen(url))
-        #dom = minidom.parse(open('queues.xml', 'r'))
-    except Exception:
-        pass
-    return dom
+    def fetch_info(self):
+        """Connect to ActiveMQ admin webpage and return DOM object"""
+        url = 'http://%s:%s/admin/xml/queues.jsp' % (self.amq_admin_host, self.amq_admin_port)
+        dom = None
+        try:
+            dom = minidom.parse(urllib.urlopen(url))
+            #dom = minidom.parse(open('queues.xml', 'r'))
+        except Exception:
+            pass
+        return dom
 
 
-def read_callback():
-    """Collectd read callback"""
-    log_verbose('Read callback called')
-    dom = fetch_info()
-    if not dom:
-        log_verbose('activemq_info plugin: No info received, offline node or turned off ActiveMQ')
-        return
+    def read_callback(self):
+        """Collectd read callback"""
+        self.log_verbose('Read callback called')
+        dom = self.fetch_info()
+        if not dom:
+            self.log_verbose('activemq_info plugin: No info received, offline node or turned off ActiveMQ')
+            return
 
-    queuenodes = dom.getElementsByTagName("queue")
-    for node in queuenodes:
-        queue = node.attributes.item(0).value
-        size = node.getElementsByTagName("stats").item(0).getAttribute("size")
-        consumerCount = node.getElementsByTagName("stats").item(0).getAttribute("consumerCount")
-        enqueueCount = node.getElementsByTagName("stats").item(0).getAttribute("enqueueCount")
-        dequeueCount = node.getElementsByTagName("stats").item(0).getAttribute("dequeueCount")
-        dispatch_value(queue, 'size',          size,          'gauge')
-        dispatch_value(queue, 'consumerCount', consumerCount, 'gauge')
-        dispatch_value(queue, 'enqueueCount',  enqueueCount,  'counter')
-        dispatch_value(queue, 'dequeueCount',  dequeueCount,  'counter')
+        queuenodes = dom.getElementsByTagName("queue")
+        for node in queuenodes:
+            queue = node.attributes.item(0).value.replace('.', '_')
+            size = node.getElementsByTagName("stats").item(0).getAttribute("size")
+            consumerCount = node.getElementsByTagName("stats").item(0).getAttribute("consumerCount")
+            enqueueCount = node.getElementsByTagName("stats").item(0).getAttribute("enqueueCount")
+            dequeueCount = node.getElementsByTagName("stats").item(0).getAttribute("dequeueCount")
+            self.dispatch_value(queue, 'gauge',   'size',          size)
+            self.dispatch_value(queue, 'gauge',   'consumerCount', consumerCount)
+            self.dispatch_value(queue, 'counter', 'enqueueCount',  enqueueCount)
+            self.dispatch_value(queue, 'counter', 'dequeueCount',  dequeueCount)
 
 
+amq = AMQMonitor()
 # register callbacks
-collectd.register_config(configure_callback)
-collectd.register_read(read_callback)
+collectd.register_config(amq.configure_callback)
+collectd.register_read(amq.read_callback)
